a6315922ef0d783739d8e1a46117848ed4b1949d
Joe Westersund <joe_westersund@hotmail.com>
2021-10-28 21:48:16 -0700

    add index

--- db/migrate/20211028042718_create_workouts.rb
+++ db/migrate/20211028042718_create_workouts.rb
@@ -4,9 +4,13 @@ class CreateWorkouts < ActiveRecord::Migration[6.1]
       t.belongs_to :user
       t.belongs_to :workout_type
       t.datetime :workout_date
+      t.int :year
+      t.int :month
+      t.int :week
 
       t.timestamps
     end
-    add_index :workouts, [:user_id, :workout_date]
+    add_index :workouts, [:user_id, :workout_date], order: {user_id: :asc, workout_date: :desc}
+    add_index :workouts, [:user_id, :year, :month, :week], order: {user_id: :asc, year: :desc, month: :desc, week: :desc}
   end
 end
